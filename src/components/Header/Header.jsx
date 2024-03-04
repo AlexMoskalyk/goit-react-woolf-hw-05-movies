bd51eae1c2ad4fdc1eb7daada2516b4b09ef104f
Oleksandr Moskalyk <alex.moskalik@gmail.com>
2024-03-04 22:45:26 +0100

    fix active link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.scss';
 const Header = () => {
   return (
     <nav className="navigation">
-      <NavLink className="link" end to="/goit-react-woolf-hw-05-movies">
+      <NavLink className="link" end to="/goit-react-woolf-hw-05-movies/">
         Home
       </NavLink>
       <NavLink className="link" to="/goit-react-woolf-hw-05-movies/Movie">
